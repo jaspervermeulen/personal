289821e6d0df0e8ce9768e5ef26903dd67f2ee57
Jasper Vermeulen <jaspervermeulen@icloud.com>
2021-11-22 21:57:14 +0100

    add wesbos linter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './index.css';
 
+import Profile from './person.png';
+
 function App() {
   return (
-    <div className="bg-gray-500">
-     
-       <p>Hello World</p>
-       
+    <div className="w-1/2 ml-32 mt-32">
+      <img src={Profile} alt="Jasper Vermeulen Profile" width="70" />
+      <p className="pt-8 pb-2 font-semibold">Jasper Vermeulen</p>
+      <p className="pb-8">
+        <span className="font-serif">Digital designer and developer. </span>
+        Building and designing mobile and web experiences. Thinking about UX/UI,
+        JavaScript frameworks, UI frameworks and so on. JavaScript developer at
+        Wheelhouse working on a variety of projects across Belgium.
+      </p>
+
+      <p>Selected Work</p>
+      <p>Insights</p>
     </div>
   );
 }
